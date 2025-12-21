84dd9af7b304ccf7f89f429f8155a5677eb77967
jackhuang0706 <2007jackhuang@gmail.com>
2025-12-21 16:10:23 +0800

    fix: improve articleHtml cleanup to remove all \n characters

--- scripts/build.js
+++ scripts/build.js
@@ -201,12 +201,13 @@ async function build() {
     for (const article of list) {
       const toc = extractTOC(article.content);
       let articleHtml = marked.parse(article.content, { mangle: false, langPrefix: "hljs language-" });
-      // 移除所有 \n 字符（字面的反斜線+n 以及實際的換行）
+      // 徹底移除所有形式的 \n 字符
       articleHtml = articleHtml
         .replace(/\\n/g, '')  // 移除字面的 \n（反斜線+n）
-        .replace(/(<\/h[1-6]>)\s*\\?n/g, '$1') // 移除 h 標籤後的 \n
-        .replace(/(<\/h[1-6]>)(\r?\n)+(<p|<ul|<ol|<h|<div|<table)/g, '$1$3') // 移除標籤間的多餘換行
-        .replace(/>\s*\n\s*</g, '><'); // 移除標籤間的換行符
+        .replace(/<p>\\n<\/p>/g, '') // 移除包含 \n 的空段落
+        .replace(/<p>\s*\\n\s*<\/p>/g, '') // 移除包含 \n 和空白的段落
+        .replace(/>\s*\\n\s*</g, '><') // 移除標籤間的 \n
+        .replace(/(<\/h[1-6]>)\s*<p>\\n<\/p>/g, '$1') // 移除標題後緊接的 \n 段落
       const html = await ejs.renderFile(
         articleTpl,
         {
